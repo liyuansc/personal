703b0f53c4a672fdeb0ade8c23a6154e35173dcc
liyu <315071659@qq.com>
2018-09-27 10:33:24 +0800

    123

--- spring-cloud/registry/src/main/java/com/liyu/personal/spring/cloud/simple/registry/RegistryApplication.java
+++ spring-cloud/registry/src/main/java/com/liyu/personal/spring/cloud/simple/registry/RegistryApplication.java
@@ -2,8 +2,10 @@ package com.liyu.personal.spring.cloud.simple.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class RegistryApplication {
 
     public static void main(String[] args) {
